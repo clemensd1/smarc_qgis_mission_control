5ec15fac782cef2c1e189253ea87b2ca8cbd2973
Wiktor D. <5301056+JadedTuna@users.noreply.github.com>
2026-07-27 18:53:55 +0200

    Update waypoint editors for named fields.
    - Bind waypoint models and widgets to a specific task field instead of
      assuming the field is named `waypoint` or `waypoints`.
    - Pass field names through waypoint editing signals and update task
      creation to use `TaskRegistry`.

--- src/ui/widgets/AddTaskDialog.py
+++ src/ui/widgets/AddTaskDialog.py
@@ -1,6 +1,6 @@
 from qgis.PyQt.QtWidgets import QWidget, QDialog
 
-from ...domain.tasks import Task, TaskType, TaskRegistry
+from ...domain.tasks import TaskRegistry
 
 from ..generated.AddTaskDialogUi import Ui_AddTaskDialog
 
@@ -13,8 +13,8 @@ class AddTaskDialog(QDialog, Ui_AddTaskDialog):
         self.setupUi(self)
 
         # Populate task type list
-        for typeName in map(str, TaskType):
-            self.taskType.addItem(typeName)
+        for typeId in TaskRegistry.registry.keys():
+            self.taskType.addItem(typeId)
 
         # Disable resizing, ensuring size provided in QtDesigner
         self.setFixedSize(self.size())
@@ -22,5 +22,5 @@ class AddTaskDialog(QDialog, Ui_AddTaskDialog):
     def description(self) -> str:
         return self.taskDescription.text().strip()
 
-    def type(self) -> TaskType:
-        return TaskType(self.taskType.currentText())
+    def type(self) -> str:
+        return self.taskType.currentText()
