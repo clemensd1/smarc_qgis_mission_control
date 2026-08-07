0a107c7fd601400b61c59a7e6d4ab6ce0733b645
clemensd1 <clemensd@kth.se>
2026-08-07 11:54:14 +0200

    Update MissionTracks.py

--- src/mission/MissionTracks.py
+++ src/mission/MissionTracks.py
@@ -191,6 +191,17 @@ class MissionTracks(QObject):
         settings.isExpression = True
         settings.setFormat(fmt)
 
+        settings.placement = Qgis.LabelPlacement.Line
+        settings.placementFlags = Qgis.LabelLinePlacementFlag.AboveLine
+        settings.distUnits = Qgis.RenderUnit.Millimeters
+        settings.dist = 2.0
+
+        settings.addDirectionSymbol = True
+        settings.leftDirectionSymbol = chr(57983)
+        settings.rightDirectionSymbol = chr(57982)
+        settings.placeDirectionSymbol = QgsPalLayerSettings.SymbolLeftRight
+        settings.reverseDirectionSymbol = False
+
         labeling = QgsVectorLayerSimpleLabeling(settings)
         self._layer.setLabeling(labeling)
 
